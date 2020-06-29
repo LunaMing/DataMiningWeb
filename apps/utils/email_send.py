1d1265e9ad53ccfbe340095a544d861bfeda0c61
Yueming Liu <44162157+LunaMing@users.noreply.github.com>
2020-06-29 18:23:29 +0800

    去掉了邮件里的NBA

--- apps/utils/email_send.py
+++ apps/utils/email_send.py
@@ -38,7 +38,7 @@ def send_register_eamil(email, send_type="register"):
     email_body = ""
 
     if send_type == "register":
-        email_title = "NBA注册激活链接"
+        email_title = "注册激活链接"
         email_body = "请点击下面的链接激活你的账号: http://127.0.0.1:8000/active/{0}".format(code)
 
         # 使用Django内置函数完成邮件发送。四个参数：主题，邮件内容，从哪里发，接受者list
@@ -48,7 +48,7 @@ def send_register_eamil(email, send_type="register"):
             pass
 
     elif send_type == "forget":
-        email_title = "NBA找回密码链接"
+        email_title = "找回密码链接"
         email_body = "请点击下面的链接找回你的密码: http://127.0.0.1:8000/reset/{0}".format(code)
 
         # 使用Django内置函数完成邮件发送。四个参数：主题，邮件内容，从哪里发，接受者list
@@ -58,7 +58,7 @@ def send_register_eamil(email, send_type="register"):
             pass
 
     elif send_type == "update_email":
-        email_title = "NBA邮箱修改验证码"
+        email_title = "邮箱修改验证码"
         email_body = "你的邮箱验证码为{0}".format(code)
 
         # 使用Django内置函数完成邮件发送。四个参数：主题，邮件内容，从哪里发，接受者list
